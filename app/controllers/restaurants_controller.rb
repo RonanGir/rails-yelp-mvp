ee2d400dbb2abd2b7cd62e7ebb6513ab093d105e
Ronan Girault <girault.ronan@gmail.com>
2017-11-22 15:56:40 +0100

    Add html and css to index and show view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,11 +6,30 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+
+  end
+
+  def new
+    @restaurant = Restaurant.new(restaurant_params)
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
   end
 end
